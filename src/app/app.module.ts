8a93b2d6f3f85ce5f7562b44e047765d877f6917
Pierluigi Ingrosso <pierluigi.ingrosso@mtmprojectsrlmonopoli.onmicrosoft.com>
2020-10-05 16:30:51 +0200

    Promise Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -63,7 +63,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     MatSelectModule,
     MatSlideToggleModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
   ],
   providers: [
     DishService,
