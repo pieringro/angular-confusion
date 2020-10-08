e912cf1e968912baec011fb2ed803de500e1ebd3
Pierluigi Ingrosso <pierluigi.ingrosso@mtmprojectsrlmonopoli.onmicrosoft.com>
2020-10-08 15:36:20 +0200

    Animations Part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { flyInOut } from '../animations/app.animation';
 
 import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut()
+  ]
 })
 export class ContactComponent implements OnInit {
 
@@ -88,7 +96,7 @@ export class ContactComponent implements OnInit {
   }
 
   onSubmit() {
-    if(this.feedbackForm.valid){
+    if (this.feedbackForm.valid) {
       this.feedback = this.feedbackForm.value;
       console.log(this.feedback);
       this.feedbackForm.reset({
--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -7,24 +7,15 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { formatDate, Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { trigger, state, style, animate, transition } from '@angular/animations'
+import { expand, visibility } from '../animations/app.animation';
 
 @Component({
   selector: 'app-dish-detail',
   templateUrl: './dish-detail.component.html',
   styleUrls: ['./dish-detail.component.scss'],
   animations: [
-    trigger('visibility', [
-      state('shown', style({
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility(),
+    expand()
   ]
 })
 export class DishDetailComponent implements OnInit {
